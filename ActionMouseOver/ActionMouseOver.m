62d8f2b2a133d31368df8a185aa9545bdbd55f10
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-18 21:07:24 -0800

    stubbed in

---
+++ ActionMouseOver/ActionMouseOver.m
@@ -0,0 +1,27 @@
+//
+//  ActionMouseOver.m
+//  ActionMouseOver
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "ActionMouseOver.h"
+
+@implementation ActionMouseOver
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	
+    if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
+    {
+		[self setName: @"MouseOver"];
+    }
+    return self;
+}
+
+
+- (BOOL) hasSelectorField {
+	return YES;
+}
+
+@end
